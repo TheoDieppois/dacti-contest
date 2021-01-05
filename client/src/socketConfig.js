2854277b59667b0e876dbf6e5d25e351d4b61330
Théo Dieppois <contact@theodieppois.fr>
2021-01-05 17:34:46 +0100

    heroku

--- client/src/socketConfig.js
+++ client/src/socketConfig.js
@@ -1,5 +1,8 @@
 import openSocket from 'socket.io-client';
+
 const socket = openSocket('https://dacti-contest.herokuapp.com/', {
   transports: ['websocket'],
+  upgrade: false,
 });
+
 export default socket;
